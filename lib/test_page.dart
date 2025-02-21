3849b9f8fbbf329b711fcfb49a9439aa4dd455b6
Timothy Bastow <timothy.bastow@outlook.com>
2025-02-21 18:10:22 +0100

    added same behaviour to test page for consistency.

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lookapp/widgets/layout/no_connection_screen.dart';
+import 'package:lookapp/providers/connection_provider.dart';
 
-class TestPage extends StatelessWidget {
+class TestPage extends ConsumerWidget {
   const TestPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isConnected = ref.watch(connectionProvider);
+
+    // If there's no internet connection, show the no connection screen
+    if (!isConnected) {
+      return const NoConnectionScreen();
+    }
+
     return const Center(child: Text('Test'));
   }
 }
